d8598e606e57ac857986c5a674d41af6ad5719d8
Starliege <urrianif@gmail.com>
2022-09-06 15:22:08 +0200

    done

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -3,16 +3,32 @@
     <div class="jumbo"></div>
     <div class="container">
       <div class="content">
-        <h2> Content goes here </h2>
+        <h2>Current Series</h2>
+        <ComicProduct
+          v-for="(element, index) in comicsArr"
+          :key="index"
+          :comic="element"
+        />
       </div>
+      <a href="#" class="load-more">Load more</a>
     </div>
   </main>
 </template>
 
 <script>
+import ComicsData from "../assets/data/dc-comics.json";
+import ComicProduct from "./commons/ComicProduct.vue";
 
 export default {
   name: "PageMain",
+  components: {
+    ComicProduct
+},
+  data() {
+    return {
+      comicsArr: ComicsData,
+    };
+  },
 };
 </script>
 
@@ -21,8 +37,54 @@ export default {
 
 main {
   background: #1c1c1c;
-  min-height: 120px;
-  justify-content: center;
-  display: flex;
+
+  .jumbo {
+    height: 500px;
+    width: 100%;
+    background: url("../assets/img/jumbotron.jpg");
+    background-size: cover;
+  }
+
+  .container {
+    position: relative;
+    display: block;
+    text-align: center;
+    padding-bottom: 100px;
+
+    .content {
+      width: 100%;
+      padding: 100px 0;
+      display: flex;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      gap: 100px 50px;
+
+      h2 {
+        position: absolute;
+        top: -25px;
+        left: -25px;
+        padding: 10px 25px;
+        width: fit-content;
+        background: $accent;
+        text-transform: uppercase;
+      }
+    }
+
+    a.load-more {
+      padding: 15px 70px;
+      width: max-content;
+      background: $accent;
+      font-family: "Roboto Condensed", sans-serif;
+      font-size: 1.5rem;
+      text-transform: uppercase;
+      text-decoration: none;
+      color: #fff;
+
+      &:hover {
+        background: #1990ff;
+      }
+    }
+  }
 }
 </style>
+
---
+++ src/components/commons/ComicProduct.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="comic">
+    <img :src="comic.thumb" :alt="comic.series" />
+    <h5>{{ comic.series }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComicProduct",
+  props: {
+    comic: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.comic {
+  width: 200px;
+  height: fit-content;
+  text-align: left;
+
+  img {
+    margin-bottom: 15px;
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    object-position: 0 0;
+  }
+
+  h5 {
+    font-size: 0.9rem;
+    font-family: "Roboto", sans-serif;
+    font-weight: normal;
+    text-transform: uppercase;
+  }
+}
+</style>
